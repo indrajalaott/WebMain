04e4543b934fcf4e363179784f928aeb6cb44bd6
dinosircode <sreejeshrayirath@gmail.com>
2024-09-12 20:27:16 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -33,23 +33,23 @@ const App = () => {
           <Route path="/forgot-password" element={<ForgotPassword />} />
         
 
-          {/* <Route path="/Subscribe" element={<PayScreen />} />
+          <Route path="/Subscribe" element={<PayScreen />} />
           <Route path="/checkout/:id" element={<Checkout />} /> 
           <Route path="/pay/:id" element={<RazorPay />} /> 
           <Route path="/Broke" element={<Broke />} /> 
           <Route path="/Profile" element={<Profile />} /> 
-           */}
+          
 
 
 
           <Route path="/Home" exact element={<Indrajala />} />
           <Route path="*" element={<Indrajala />} />
           
-{/*           
+          
             <Route path="/Movie/" element={<MoviePage />} />    
           <Route path="/movie/:url" element={<MovieDetailPage />} />
           <Route path="/trailer/*" element={<TrailerPage />} /> 
-          <Route path="/trailer/:movieId" element={<TrailerPage />} /> */}
+          <Route path="/trailer/:movieId" element={<TrailerPage />} />
 
     
           
