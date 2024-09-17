5b77ec461640ae4e099526030d6d39c13998c600
dinosircode <sreejeshrayirath@gmail.com>
2024-09-17 20:53:28 +0530

    Added API

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,6 +5,30 @@ export const fetchMovies = async () => {
   };
 
 
+  export const toptrending = async () => {
+    const response = await fetch('https://api.indrajala.in/api/admin/toptrending');
+    const data = await response.json();
+    return data;
+  };
+
+
+
+  export const topfive = async () => {
+    const response = await fetch('https://api.indrajala.in/api/admin/topfive');
+    const data = await response.json();
+    return data;
+  };
+
+
+  export const upcomming = async () => {
+    const response = await fetch('https://api.indrajala.in/api/admin/upcomming');
+    const data = await response.json();
+    return data;
+  };
+
+
+
+
 export const HoverMovies = async () => {
     const response = await fetch('https://api.indrajala.in/api/admin/CorrosilListMovies');
     const data = await response.json();
